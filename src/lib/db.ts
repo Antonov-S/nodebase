bf32748d898fe769bd750bc93256c4d2010aa079
Antonov-S <anto1nov1@gmail.com>
2025-11-30 14:35:03 +0200

    02: database

---
+++ src/lib/db.ts
@@ -0,0 +1,31 @@
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@/generated/prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+const connectionString = process.env.DATABASE_URL!;
+if (!connectionString) {
+  throw new Error("DATABASE_URL is missing");
+}
+
+const adapter = new PrismaNeon({
+  connectionString
+});
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "info", "warn", "error"]
+        : ["error"]
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
