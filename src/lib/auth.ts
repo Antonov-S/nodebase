81fab3ecf14080ebf062d30340a4e6e4938f79e7
Antonov-S <anto1nov1@gmail.com>
2025-12-31 03:42:40 +0200

    10: payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,9 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { polar, checkout, portal } from "@polar-sh/better-auth";
 
 import prisma from "@/lib/db";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,5 +12,24 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     autoSignIn: true
-  }
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "ca5c3022-909d-4efd-afd4-4faf2782b379",
+              slug: "pro"
+            }
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true
+        }),
+        portal()
+      ]
+    })
+  ]
 });
